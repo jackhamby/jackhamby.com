3c1d597499e333db4de9925f8133d08c327750e8
Jack Hamby <jackhamby@Sansoros-MacBook-Pro-8.local>
2019-07-01 12:02:41 -0500

    added resume

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
-    # return 'Hello, World!'
 
-
-
+@app.route('/resume')
+def resume():
+    return render_template("resume.html")
 
 
 if __name__ == "__main__":
