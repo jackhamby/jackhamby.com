19f926037d7f84f169be6f4ddd465cc8561a087f
JackHamby <jackhamby@JackHambys-MacBook-Pro.local>
2020-05-07 23:34:43 -0500

    test endpoints

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
+
+server_data = {}
 
 @app.route('/')
 def index():
@@ -11,6 +13,22 @@ def index():
 def resume():
     return render_template("resume.html")
 
+@app.route('/cheese/<key>', methods=['PUT'])
+def set_data(key):
+    try:
+        data = request.get_json()
+        server_data[key] = data["value"]
+    except Exception as e:
+        return {'error' : str(e)}
+    return {
+        'success' : f'set {key} to value {data["value"]}'
+    }
+
+@app.route('/cheese/<key>', methods=['GET'])
+def get_data(key):
+    response = {}
+    response[key] = server_data[key]
+    return response
 
 if __name__ == "__main__":
     # app.run()
